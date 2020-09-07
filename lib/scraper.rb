05efeca99947d3946cfafb5a94fd652c1fb0ecb9
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-07 08:01:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,8 +8,6 @@ class Scraper
   def self.scrape_index_page(index_url)
     
     resp = Nokogiri::HTML(open(index_url))
-    
-    # stu_hash = JSON.parse(resp.body, symbolize_names:true)
     # student_arr = student_hash[:student]
     #     student_arr.collect do | student_hash|
     #         Student.new(student_hash)
