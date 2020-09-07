ace3463cbc4c1c73698bf2222780d24d328b7dd5
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-07 08:04:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
     
     resp = Nokogiri::HTML(open(index_url))
     student_arr = resp.css('div.student-card')
-    binding.pry
+y
   end
 
   def self.scrape_profile_page(profile_url)
