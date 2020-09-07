688c4c0b1d4b6c03a5fe1ade8de1793709827d54
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-07 08:23:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,10 +23,12 @@ class Scraper
     resp = Nokogiri::HTML(open(profile_url))
     social_link = resp.css('div.social-icon-container a')
     social_link.collect do |student|
-      { twitter:
-      :linkedin
-       :github
-       :profile_quote
+      { twitter: student.css('div.social-icon-container a')
+      linkedin: student.css('div.social-icon-container a')
+       github: student.css('div.social-icon-container a')
+       profile_quote: student.css('div.social-icon-container a')
+       bio: student.css('div.social-icon-container a')
+      }
   end
 
 end
